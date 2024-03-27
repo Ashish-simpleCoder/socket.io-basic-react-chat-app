cc315ac4e2745816c7731dd291bfa3e1274b61f7
Ashish Prajapati <62009244+Ashish-simpleCoder@users.noreply.github.com>
2024-03-27 10:16:28 +0000

    refactor: store socket ref in use-memo hook in react

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -10,7 +10,7 @@ import MessageContainer from './components/shared/message-container'
 export type Msg = { msg: string; sender: string; current?: boolean; AvatarClr: string; timestamp: number }
 
 export default function App() {
-   const socket = useRef(io('http://localhost:3000'))
+   const socket = useMemo(() =>io('http://localhost:3000'),[])
    const [isJoined, setIsJoined] = useState(false)
    const [msgs, setMsgs] = useState<Array<Msg>>([])
    const msgContainerRef = useRef<ElementRef<'div'>>(null)
@@ -18,10 +18,10 @@ export default function App() {
    const AvatarClr = useMemo(() => '#' + Math.random().toString(16).slice(-6), [])
 
    const handleSubmit = ({ room, username }: { room: string; username: string }) => {
-      socket.current.emit('join_room', { room, username, AvatarClr })
+      socket.emit('join_room', { room, username, AvatarClr })
    }
    const handleSendMsg = (msg: string) => {
-      socket.current.emit('send_msg', msg)
+      socket.emit('send_msg', msg)
       flushSync(() => {
          setMsgs((old_msgs) => {
             old_msgs.push({ msg, sender: username, current: true, AvatarClr, timestamp: Date.now() })
@@ -32,18 +32,18 @@ export default function App() {
    }
 
    useEffect(() => {
-      // socket.current.on('connect', () => {
-      //    console.log('connected', socket.current.id)
+      // socket.on('connect', () => {
+      //    console.log('connected', socket.id)
       // })
 
-      // socket.current.on('welcome', (msg) => {
+      // socket.on('welcome', (msg) => {
       //    console.log({ msg })
       // })
-      socket.current.on('join_successful', (msg) => {
+      socket.on('join_successful', (msg) => {
          toast.success(msg)
          setIsJoined(true)
       })
-      socket.current.on('recieve_msg', (data) => {
+      socket.on('recieve_msg', (data) => {
          flushSync(() => {
             setMsgs((old_msgs) => {
                old_msgs.push(data)
@@ -53,7 +53,7 @@ export default function App() {
          msgContainerRef.current?.scrollTo(0, msgContainerRef.current.scrollHeight)
       })
       return () => {
-         socket.current.disconnect()
+         socket.disconnect()
          setIsJoined(false)
       }
    }, [])
